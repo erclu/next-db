34f0ca872d1b5a3d82eea666f7c91a3642151aed
Marco Ferrati <marco.ferrati@gmail.com>
2019-01-11 22:02:18 +0000

    Aggiunte le intestazioni per tutte le 13 tabelle

--- database.sql
+++ database.sql
@@ -1,1 +1,52 @@
 #Progetto Base di Dati 18/19 Ercole Luca e Ferrati Marco
+
+DROP TABLE IF EXIST Metodi_di_pagamento;
+CREATE TABLE Metodi_di_pagamento()
+
+
+DROP TABLE IF EXIST Utenti;
+CREATE TABLE Utenti()
+
+
+DROP TABLE IF EXIST Richieste;
+CREATE TABLE Richieste()
+
+
+DROP TABLE IF EXIST Tariffe;
+CREATE TABLE Tariffe()
+
+
+DROP TABLE IF EXIST Corse;
+CREATE TABLE Richieste()
+
+
+DROP TABLE IF EXIST Storici;
+CREATE TABLE Richieste()
+
+
+DROP TABLE IF EXIST Partecipazioni;
+CREATE TABLE Partecipazioni()
+
+
+DROP TABLE IF EXIST Tratte;
+CREATE TABLE Tratte()
+
+
+DROP TABLE IF EXIST Fermate;
+CREATE TABLE Fermate()
+
+
+DROP TABLE IF EXIST Indicazioni_tratte;
+CREATE TABLE Indicazioni_tratte()
+
+
+DROP TABLE IF EXIST Veicoli;
+CREATE TABLE Veicoli()
+
+
+DROP TABLE IF EXIST Autisti;
+CREATE TABLE Autisti()
+
+
+DROP TABLE IF EXIST Hub_di_ricarica
+CREATE TABLE Hub_di_ricarica()
