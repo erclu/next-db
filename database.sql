0b8d16959df25132af53627803e5dbe83d465c30
mferrati <mferrati@dip050.studenti.math.unipd.it>
2019-01-17 11:10:09 +0100

    Aggiunti i metodi di pagamento

--- database.sql
+++ database.sql
@@ -17,10 +17,10 @@ CREATE TABLE Utenti(
 
 DROP TABLE IF EXISTS Fonti_di_pagamento;
 CREATE TABLE Fonti_di_pagamento(
+	Id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	Utente INTEGER,
-	Tipo VARCHAR(50),
+	Tipo VARCHAR(255),
 
-	PRIMARY KEY (Utente, Tipo), -- Solo un tipo per utente?
 	FOREIGN KEY(Utente) REFERENCES Utenti(Id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -184,3 +184,30 @@ CREATE TABLE Archi(
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 SET FOREIGN_KEY_CHECKS=1;
+
+-- Inserimento dei dati
+INSERT INTO Fonti_di_pagamento(Id, Utente, Tipo) VALUES
+(NULL, 1, "Carta di credito"),
+(NULL, 1, "PayPal"),
+(NULL, 2, "Carta di credito"),
+(NULL, 2, "ApplePay"),
+(NULL, 3, "Carta di credito"),
+(NULL, 3, "PayPal"),
+(NULL, 4, "Carta di credito"),
+(NULL, 4, "GooglePay"),
+(NULL, 5, "Carta di credito"),
+(NULL, 5, "ApplePay"),
+(NULL, 6, "Carta di credito"),
+(NULL, 6, "GooglePay"),
+(NULL, 7, "Carta di credito"),
+(NULL, 7, "PayPal"),
+(NULL, 8, "Carta di credito"),
+(NULL, 8, "ApplePay"),
+(NULL, 9, "Carta di credito"),
+(NULL, 9, "PayPal"),
+(NULL, 10, "Carta di credito"),
+(NULL, 10, "GooglePay");
+
+-- Query
+
+--
