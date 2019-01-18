f5a97ac80eb5f093bac9dfba430ec12bb9cedf20
erclu <30255227+erclu@users.noreply.github.com>
2019-01-18 16:57:45 +0100

    fix dimensione utenti.email + altro

--- create.sql
+++ create.sql
@@ -6,7 +6,7 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS Utenti;
 CREATE TABLE Utenti(
 	Id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	Email VARCHAR(255) NOT NULL UNIQUE,
+	Email VARCHAR(191) NOT NULL UNIQUE, /* 191 dimensione massima per indice su varchar con encoding utf8mb4! */
 	Password CHAR(64) NOT NULL,
 	Nome VARCHAR(255) NOT NULL,
 	Cognome VARCHAR(255) NOT NULL,
@@ -179,34 +179,13 @@ CREATE TABLE Archi(
 	Nome VARCHAR(255),
 	Peso INTEGER NOT NULL,
 
+  PRIMARY KEY (Entrante, Uscente),
 	FOREIGN KEY(Entrante) REFERENCES Nodi(Id),
 	FOREIGN KEY(Uscente) REFERENCES Nodi(Id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 SET FOREIGN_KEY_CHECKS=1;
 
--- Inserimento dei dati
-INSERT INTO Metodi_di_pagamento(Id, Utente, Tipo) VALUES
-(NULL, 1, "Carta di credito"),
-(NULL, 1, "PayPal"),
-(NULL, 2, "Carta di credito"),
-(NULL, 2, "ApplePay"),
-(NULL, 3, "Carta di credito"),
-(NULL, 3, "PayPal"),
-(NULL, 4, "Carta di credito"),
-(NULL, 4, "GooglePay"),
-(NULL, 5, "Carta di credito"),
-(NULL, 5, "ApplePay"),
-(NULL, 6, "Carta di credito"),
-(NULL, 6, "GooglePay"),
-(NULL, 7, "Carta di credito"),
-(NULL, 7, "PayPal"),
-(NULL, 8, "Carta di credito"),
-(NULL, 8, "ApplePay"),
-(NULL, 9, "Carta di credito"),
-(NULL, 9, "PayPal"),
-(NULL, 10, "Carta di credito"),
-(NULL, 10, "GooglePay");
 -- Query
 
 --
--- load_data.sql
+++ load_data.sql
@@ -2,16 +2,47 @@
 -- SET FOREIGN_KEY_CHECKS=0;
 
 LOAD DATA LOCAL INFILE 'data/Utenti.csv' INTO TABLE Utenti
-  FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"'
-  LINES TERMINATED BY '\n';
+  (Email, Password, Nome, Cognome, Data_di_nascita)
+  FIELDS TERMINATED BY ','
+  OPTIONALLY ENCLOSED BY '"'
+  ESCAPED BY '\\'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES;
+
+INSERT INTO Metodi_di_pagamento(Id, Utente, Tipo) VALUES
+(NULL, 1, "Carta di credito"),
+(NULL, 1, "PayPal"),
+(NULL, 2, "Carta di credito"),
+(NULL, 2, "ApplePay"),
+(NULL, 3, "Carta di credito"),
+(NULL, 3, "PayPal"),
+(NULL, 4, "Carta di credito"),
+(NULL, 4, "GooglePay"),
+(NULL, 5, "Carta di credito"),
+(NULL, 5, "ApplePay"),
+(NULL, 6, "Carta di credito"),
+(NULL, 6, "GooglePay"),
+(NULL, 7, "Carta di credito"),
+(NULL, 7, "PayPal"),
+(NULL, 8, "Carta di credito"),
+(NULL, 8, "ApplePay"),
+(NULL, 9, "Carta di credito"),
+(NULL, 9, "PayPal"),
+(NULL, 10, "Carta di credito"),
+(NULL, 10, "GooglePay");
 
 LOAD DATA LOCAL INFILE 'data/Nodi.csv' INTO TABLE Nodi
-  FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"'
-  
-  LINES TERMINATED BY '\n';
+  FIELDS TERMINATED BY ','
+  OPTIONALLY ENCLOSED BY '"'
+  ESCAPED BY '\\'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES;
 
 LOAD DATA LOCAL INFILE 'data/Archi.csv' INTO TABLE Archi
-  FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"'
-  LINES TERMINATED BY '\n';
+  FIELDS TERMINATED BY ','
+  OPTIONALLY ENCLOSED BY '"'
+  ESCAPED BY '\\'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES;
 
 -- SET FOREIGN_KEY_CHECKS=1;
