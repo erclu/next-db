f78806933e0501211cc6f2c1aed85323f7fed893
erclu <30255227+erclu@users.noreply.github.com>
2019-01-25 15:54:31 +0100

    varie

--- create.sql
+++ create.sql
@@ -1,4 +1,4 @@
-/*Progetto Base di Dati 18/19 Ercole Luca e Ferrati Marco*/
+/* Progetto Base di Dati 18/19 Ercole Luca e Ferrati Marco*/
 
 /* drop di tutte le tabelle */
 
@@ -6,19 +6,19 @@ SET FOREIGN_KEY_CHECKS=0;
 
 DROP TABLE IF EXISTS Archi, Associazioni, Autisti, Corse, Eventi, Indicazioni, Metodi_di_pagamento, Nodi, Richieste, Stazioni_di_ricarica, Storico_corse, Storico_tratte, Tratte, Utenti, Veicoli;
 
--- SET FOREIGN_KEY_CHECKS=1;
+SET FOREIGN_KEY_CHECKS=1;
 
-/*Creazione tabelle e inserimento valori*/
+/* Creazione tabelle e inserimento valori */
 
 CREATE TABLE Utenti(
   Id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  Email VARCHAR(190) NOT NULL UNIQUE, /* 191 è la dimensione massima per un indice su varchar con encoding utf8mb4! */
+  Email VARCHAR(190) NOT NULL UNIQUE, /* 191 è la dimensione massima per un indice su varchar con encoding utf8mb4 */
   Password CHAR(64) NOT NULL,
   Nome VARCHAR(255) NOT NULL,
   Cognome VARCHAR(255) NOT NULL,
   Data_di_nascita DATE NOT NULL
 ) Engine=InnoDB;
-/*Nota, all'inserimento mettere il campo password cifrato (con la funzione encode?)*/
+/* Nota, all'inserimento cifrare il campo password */
 
 CREATE TABLE Metodi_di_pagamento(
   Id INTEGER PRIMARY KEY AUTO_INCREMENT,
@@ -36,7 +36,7 @@ CREATE TABLE Corse(
   Destinazione_x INTEGER NOT NULL,
   Destinazione_y INTEGER NOT NULL,
   Ora_conclusione TIMESTAMP,
-  Prezzo DECIMAL(10,2) NOT NULL,
+  Prezzo DECIMAL(10, 2) NOT NULL,
 
   CHECK(Prezzo>0)
 ) Engine=InnoDB;
@@ -45,16 +45,16 @@ CREATE TABLE Richieste(
   Id INTEGER PRIMARY KEY AUTO_INCREMENT,
   Orario_richiesta TIMESTAMP NOT NULL,
   Orario_partenza TIMESTAMP NOT NULL,
-  Origine_x DECIMAL(3,1) NOT NULL,
-  Origine_y DECIMAL(3,1) NOT NULL,
-  Destinazione_x DECIMAL(3,1) NOT NULL,
-  Destinazione_y DECIMAL(3,1) NOT NULL,
+  Origine_x DECIMAL(3, 1) NOT NULL,
+  Origine_y DECIMAL(3, 1) NOT NULL,
+  Destinazione_x DECIMAL(3, 1) NOT NULL,
+  Destinazione_y DECIMAL(3, 1) NOT NULL,
   Accettata BOOLEAN,
   Corsa INTEGER,
   Utente INTEGER NOT NULL,
 
-  CONSTRAINT CHK_Origine CHECK(TRUNCATE(Origine_x,0)=Origine_x OR TRUNCATE(Origine_y,0)=Origine_y),
-  CONSTRAINT CHK_Destinazione CHECK(TRUNCATE(Destinazione_x,0)=Destinazione_x OR TRUNCATE(Destinazione_y,0)=Destinazione_y),
+  CHECK(TRUNCATE(Origine_x, 0)=Origine_x OR TRUNCATE(Origine_y, 0)=Origine_y),
+  CHECK(TRUNCATE(Destinazione_x, 0)=Destinazione_x OR TRUNCATE(Destinazione_y, 0)=Destinazione_y)
 
   FOREIGN KEY(Utente) REFERENCES Utenti(Id),
   FOREIGN KEY(Corsa) REFERENCES Corse(Id)
@@ -72,13 +72,13 @@ CREATE TABLE Storico_corse(
 CREATE TABLE Tratte(
   Id INTEGER PRIMARY KEY AUTO_INCREMENT,
   Orario_partenza TIMESTAMP NOT NULL,
-  Inizio_x DECIMAL(3,1) NOT NULL,
-  Inizio_y DECIMAL(3,1) NOT NULL,
-  Fine_x DECIMAL(3,1) NOT NULL,
-  Fine_y DECIMAL(3,1) NOT NULL,
+  Inizio_x DECIMAL(3, 1) NOT NULL,
+  Inizio_y DECIMAL(3, 1) NOT NULL,
+  Fine_x DECIMAL(3, 1) NOT NULL,
+  Fine_y DECIMAL(3, 1) NOT NULL,
 
-  CONSTRAINT CHK_Inizio CHECK(TRUNCATE(Inizio_x,0)=Inizio_x OR TRUNCATE(Inizio_y,0)=Inizio_y),
-  CONSTRAINT CHK_Fine CHECK(TRUNCATE(Fine_x,0)=Fine_x OR TRUNCATE(Fine_y,0)=Fine_y)
+  CHECK(TRUNCATE(Inizio_x, 0)=Inizio_x OR TRUNCATE(Inizio_y, 0)=Inizio_y),
+  CHECK(TRUNCATE(Fine_x, 0)=Fine_x OR TRUNCATE(Fine_y, 0)=Fine_y)
 ) Engine=InnoDB;
 
 CREATE TABLE Associazioni(
@@ -113,6 +113,8 @@ CREATE TABLE Indicazioni(
   Destinazione INTEGER NOT NULL,
   Tratta INTEGER NOT NULL,
 
+  CHECK(Partenza<>Destinazione),
+
   PRIMARY KEY(Partenza, Destinazione, Tratta),
   FOREIGN KEY(Partenza) REFERENCES Nodi(Id), -- vincolo partenza e destinazione diversi?
   FOREIGN KEY(Destinazione) REFERENCES Nodi(Id) -- vincolo partenza e destinazione diversi?
@@ -121,8 +123,8 @@ CREATE TABLE Indicazioni(
 CREATE TABLE Veicoli(
   Targa INTEGER PRIMARY KEY AUTO_INCREMENT,
   Stato_batteria INTEGER NOT NULL,
-  Posizione_x INTEGER NOT NULL,
-  Posizione_y INTEGER NOT NULL,
+  Posizione_x DECIMAL(3, 1) NOT NULL,
+  Posizione_y DECIMAL(3, 1) NOT NULL,
   Tipo VARCHAR(255) NOT NULL DEFAULT "Trasporto persone",
   Guidatore INTEGER,
   In_ricarica INTEGER,
@@ -130,6 +132,7 @@ CREATE TABLE Veicoli(
   Testa BOOLEAN,
 
   CHECK(Stato_batteria>=0 AND Stato_batteria<=100),
+  CHECK(TRUNCATE(Posizione_x, 0)=Posizione_x OR TRUNCATE(Posizione_y, 0)=Posizione_y),
   CHECK(Tipo="Trasporto persone" OR Tipo="Trasporto merci" OR Tipo="Servizi" OR Tipo="Battery pack"),
   CHECK((Tratta IS NULL AND TESTA IS NULL) OR (Tratta IS NOT NULL AND Testa IS NOT NULL)),
 
@@ -161,11 +164,11 @@ CREATE TABLE Storico_tratte(
 
 CREATE TABLE Stazioni_di_ricarica(
   Id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  Posizione_x DECIMAL(3,1) NOT NULL,
-  Posizione_y DECIMAL(3,1) NOT NULL,
+  Posizione_x DECIMAL(3, 1) NOT NULL,
+  Posizione_y DECIMAL(3, 1) NOT NULL,
   Posti_totali INTEGER NOT NULL,
 
-  CONSTRAINT CHK_Posizione CHECK(TRUNCATE(Posizione_x,0)=Posizione_x OR TRUNCATE(Posizione_y,0)=Posizione_y)
+  CHECK(TRUNCATE(Posizione_x, 0)=Posizione_x OR TRUNCATE(Posizione_y, 0)=Posizione_y)
 ) Engine=InnoDB;
 
 CREATE TABLE Archi(
