e5611987619b71d8517300ac852ff9295a11d4fb
erclu <30255227+erclu@users.noreply.github.com>
2019-01-26 17:58:09 +0100

    query 6

--- query.sql
+++ query.sql
@@ -1,20 +1,26 @@
 -- Query Progetto Basi di Dati 2018/19 Ercole Luca e Ferrati Marco
 
-
-insert into Richieste values (null, current_timestamp, current_timestamp, 1, 37, 1, 20, null, null, 1)
-
--- trova tutti i veicoli vicini alle richieste
+-- #6
+DROP VIEW IF EXISTS Veicoli_per_Richiesta;
+CREATE VIEW Veicoli_per_Richiesta AS
 SELECT
-    r.Id as Id_richiesta, r.Origine_x, r.Origine_y, v.Targa, v.Posizione_x, v.Posizione_y
+    r.Id AS Id_richiesta,
+    r.Origine_x AS R_x,
+    r.Origine_y AS R_y,
+    v.Targa,
+    ABS(r.Origine_x - v.Posizione_x) AS Distanza_x,
+    ABS(r.Origine_y - v.Posizione_y) AS Distanza_y,
+    v.Posizione_x AS V_x,
+    v.Posizione_y AS V_y
 FROM
     Richieste r,
     Veicoli v
 WHERE
-    v.Tipo = "Trasporto persone" AND
-    r.Accettata IS NULL AND
-    r.Origine_x BETWEEN v.Posizione_x - 25 and v.Posizione_x + 25
-    AND
-    r.Origine_y BETWEEN v.Posizione_y - 25 and v.Posizione_y + 25
+    v.Tipo = "Trasporto persone" AND r.Accettata IS NULL AND ABS(r.Origine_x - v.Posizione_x) < 25 AND ABS(r.Origine_y - v.Posizione_y) < 25
 GROUP BY
-    r.Id, v.Targa
-;
+    r.Id,
+    v.Targa
+ORDER BY
+    r.Id,
+    Distanza_x,
+    Distanza_y;
