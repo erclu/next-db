01bd4b8f78f154c5b53d338de0c55a8830dc481f
FERRATI Marco <mferrati@basidati1819.studenti.math.unipd.it>
2019-01-28 17:50:35 +0100

    Completato trigger 1 per aggiornamento prezzo

--- operations.sql
+++ operations.sql
@@ -9,17 +9,21 @@ WHERE a.Tratta = t.Id
 GROUP BY t.Id;
 
 DROP TRIGGER IF EXISTS Calcolo_prezzo;
+DELIMITER |
 CREATE TRIGGER Calcolo_prezzo
-AFTER UPDATE OF Orario_conclusione ON Corse
+BEFORE UPDATE  ON Corse
 FOR EACH ROW
-DECLARE DECIMAL(10,2) costoUnitario;
-SET costoUnitario = 1;
+BEGIN
+-- DECLARE costo_unitario  DECIMAL(10,2);
+-- SET costo_nitario = 1;
 
-DECLARE INTEGER price;
+DECLARE price DECIMAL(10,2);
 SET price = 0;
 
 SELECT SUM(lcpt.lunghezza_cammino/uit.num_posti_occupati_in_tratta) INTO price
 FROM Utenti_in_tratta uit, Associazioni a, Lunghezza_cammino_per_tratta lcpt
-WHERE new.Richiesta = a.Corsa AND a.Tratta = uit.tratta_id AND lcpt.tratta_id = uit.tratta_id;
+WHERE NEW.Richiesta = a.Corsa AND a.Tratta = uit.tratta_id AND lcpt.tratta_id = uit.tratta_id;
 
-SET NEW.prezzo = price WHERE Richiesta=NEW.Richiesta;
+SET NEW.prezzo = price;
+END |
+DELIMITER ;
