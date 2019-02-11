baf72d3722ff28d6007b6bc41f045537f978f633
Luca Ercole <30255227+erclu@users.noreply.github.com>
2019-02-11 21:29:32 +0100

    test alternative script

--- init_db_ercolel.sh
+++ init_db_ercolel.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "-- creating data"
 python3 -m data_builder
+
+alias custom_mysql='mysql -h localhost -P3306 --user="ercolel" --password=$( cat $HOME/bd1819.password ) -D ercolel-PR --default-character-set=utf8 --show-warnings'
+
 echo "-- creating tables"
-mysql -h localhost -P3306 -u ercolel -D ercolel-PR --local-infile=1 --password=$( cat $HOME/bd1819.password ) --show-warnings < create.sql
+custom_mysql < create.sql
 echo "-- loading data"
-mysql -h localhost -P3306 -u ercolel -D ercolel-PR --local-infile=1 --password=$( cat $HOME/bd1819.password ) --show-warnings < load_data.sql
+custom_mysql < load_data.sql
 echo "-- creating queries, triggers and functions"
-mysql -h localhost -P3306 -u ercolel -D ercolel-PR --local-infile=1 --password=$( cat $HOME/bd1819.password ) --show-warnings < queries.sql
-mysql -h localhost -P3306 -u ercolel -D ercolel-PR --local-infile=1 --password=$( cat $HOME/bd1819.password ) --show-warnings < operations.sql
+custom_mysql < queries.sql
+custom_mysql < operations.sql
