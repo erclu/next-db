c6ebffaec804d91ebc4a41c4d624333bf1a495dd
erclu <30255227+erclu@users.noreply.github.com>
2019-02-11 21:32:17 +0100

    removed files from my fork

--- init_db_mferrati.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "-- creating data"
-python3 -m data_builder
-echo "-- creating tables"
-mysql -h localhost -P3306 -u mferrati -D mferrati-PR --local-infile=1 --password=$( cat $HOME/bd1819.password ) --show-warnings < create.sql
-echo "-- loading data"
-mysql -h localhost -P3306 -u mferrati -D mferrati-PR --local-infile=1 --password=$( cat $HOME/bd1819.password ) --show-warnings < load_data.sql
